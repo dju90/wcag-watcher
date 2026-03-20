c3b6774383351930ec4938d165b8fdf0a91d8823
dju90 <bryan@code.org>
2026-03-20 16:34:54 -0700

    update repo name

--- vite-config.js
+++ vite-config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace 'a11y-monitor' with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/a11y-monitor/",
+  base: "/wcag-watcher/",
 });
